c03f7e6065ed2326667c9e6d49d96297d33d8919
bony075 <bony.075@gmail.com>
2022-11-28 17:22:52 +0600

    create verify token

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
     try {
         const productCollection = client.db('hotSale').collection('productCollection');
@@ -64,13 +84,12 @@ async function run() {
          })
 
         
-        app.get("/bookedProduct", async (req, res) => {
+        app.get("/bookedProduct", verifyJWT, async (req, res) => {
           const email = req.query.email;
-        //   const decodedEmail = req.decoded.email;
-
-        //   if (email !== decodedEmail) {
-        //     return res.status(403).send({ message: "forbidden access" });
-        //   }
+          const decodedEmail = req.decoded.email;
+          if (email !== decodedEmail) {
+            return res.status(403).send({ message: "forbidden access" });
+          }
 
           const query = { email: email };
           const bookings = await bookProductCollection.find(query).toArray();
